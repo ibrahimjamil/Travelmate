e4dabd52fde2d4bca681595864e60476c4e0df24
ibrahimjamil <ibrahimjamil090@gmail.com>
2023-07-13 02:52:44 +0500

    invite

--- src/modules/invite/invite.controller.ts
+++ src/modules/invite/invite.controller.ts
@@ -1,5 +1,6 @@
 import { Router, Request, Response } from 'express';
 import nodemailer from 'nodemailer';
+import {google, google} from 'googleapis'
 
 export class InviteController {
   public router: Router;
@@ -12,12 +13,25 @@ export class InviteController {
   public postEmail = async (req: Request, res: Response) => {
     const { email } = req.body;
     const testAccount = await nodemailer.createTestAccount();
-    const transporter = nodemailer.createTransport({
-        host: 'gmail',
-        port: 587,
+    const CLIENTID = '855982585807-29ct3cdn2ff42kc9j2ij90voumtvog2s.apps.googleusercontent.com'
+    const CLIENT_SECRET = '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'
+    const REDIRECT_URI = 'https://developers.google.com/oauthplayground'
+    const REFRESH_TOKEN = '1//049a9IZLSpMhECgYIARAAGAQSNwF-L9IrklDwV7L8xL8vTWj0xEzcW5Quv_o05oWv409CgLfWVkpZf1rZMCJIdZm0ou6WcGcHgK0'
+
+    const googleAPI = new google.auth.OAuth2(CLIENTID, CLIENT_SECRET, REDIRECT_URI)
+    googleAPI.setCredentials({refresh_token: REFRESH_TOKEN})
+
+    const accessToken = await googleAPI.getAccessToken();
+
+    const transporter: any = nodemailer.createTransport<any>({
+        service: 'gmail',
         auth: {
+            type: 'OAuth2',
             user: 'jacklyn.kreiger@ethereal.email',
-            pass: '144s42UK4gAHaAkGT8'
+            clientId: CLIENTID,
+            clientSecret: CLIENT_SECRET,
+            refreshToken: REFRESH_TOKEN,
+            accessToken: accessToken
         }
     }) 
 
