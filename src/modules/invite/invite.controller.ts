223d2b7d9099254238df79bd3f85f7ed379e3d6d
ibrahimjamil <ibrahimjamil090@gmail.com>
2023-07-13 03:19:12 +0500

    fix

--- src/modules/invite/invite.controller.ts
+++ src/modules/invite/invite.controller.ts
@@ -12,18 +12,26 @@ export class InviteController {
 
   public postEmail = async (req: Request, res: Response) => {
     const { email } = req.body;
-    const googleAPI = new google.auth.OAuth2(process.env.CLIENTID, process.env.CLIENT_SECRET, process.env.REDIRECT_URI)
-    googleAPI.setCredentials({refresh_token: process.env.REFRESH_TOKEN})
+    const testAccount = await nodemailer.createTestAccount();
+    const CLIENTID = '855982585807-29ct3cdn2ff42kc9j2ij90voumtvog2s.apps.googleusercontent.com'
+    const CLIENT_SECRET = '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'
+    const REDIRECT_URI = 'https://developers.google.com/oauthplayground'
+    const REFRESH_TOKEN = '1//049a9IZLSpMhECgYIARAAGAQSNwF-L9IrklDwV7L8xL8vTWj0xEzcW5Quv_o05oWv409CgLfWVkpZf1rZMCJIdZm0ou6WcGcHgK0'
+
+    const googleAPI = new google.auth.OAuth2(CLIENTID, CLIENT_SECRET, REDIRECT_URI)
+    googleAPI.setCredentials({refresh_token: REFRESH_TOKEN})
+
+    const accessToken = await googleAPI.getAccessToken();
 
     const transporter: any = nodemailer.createTransport({
         service: 'gmail',
         auth: {
             type: 'OAuth2',
             user: 'ibrahimjamil090@gmail.com',
-            clientId: process.env.CLIENTID,
-            clientSecret: process.env.CLIENT_SECRET,
-            refreshToken: process.env.REFRESH_TOKEN,
-            accessToken: process.env.ACCESS_TOKEN
+            clientId: CLIENTID,
+            clientSecret: CLIENT_SECRET,
+            refreshToken: REFRESH_TOKEN,
+            accessToken: 'ya29.a0AbVbY6NCExPVtW1FTM86UKWl8k8qUX-aXnN2owvE4qRnaC4KRKkwhh6oZklgNRYzY081IVjkDSUp3xg2zj9scbgT9dBEzcmeMhsF0cG9Qqa01-t20Hm_2a2TK4s6Iy0gOINgJxasBbtxlFhryWJ3uwFvMSYIaCgYKAfsSARISFQFWKvPlcTWswLvjsyg5qv24dV2lzw0163'
         }
     }) as any
 
@@ -31,7 +39,7 @@ export class InviteController {
         from: 'ibrahimjamil090@gmail.com',
         to: `${email}`,
         subject: "Travelmate Invite",
-        text: "Hy! I invite you to our app travelmate for travel user matching online visit this website: https://travelmate-frontend.vercel.app/",
+        text: "Hy! I invite you to our app travelmate for travel user matching online this website https://travelmate-frontend.vercel.app/",
         html: "<b>Hy! I invite you to our app travelmate for travel user matching online</b>",
       });
 
