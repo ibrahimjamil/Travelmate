92f23a08cacb019a0708c9882efb26332e56fa04
ibrahimjamil <ibrahimjamil090@gmail.com>
2023-07-14 01:30:39 +0500

    call feature

--- src/socket.ts
+++ src/socket.ts
@@ -59,7 +59,19 @@ function socket({ io }: { io: Server }) {
     });
   
     socket.on("disconnect", () => {
+      socket?.broadcast?.emit("callEnded")
       console.log("a user disconnected");
+    });
+
+    // video call
+    socket.emit("me", socket.id);
+
+    socket.on("callUser", ({ userToCall, signalData, from, name }) => {
+      io.to(userToCall).emit("callUser", { signal: signalData, from, name });
+    });
+  
+    socket.on("answerCall", (data) => {
+      io.to(data.to).emit("callAccepted", data.signal)
     });
   });
 }
