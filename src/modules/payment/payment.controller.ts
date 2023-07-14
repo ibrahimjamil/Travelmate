5fe9a05b3f3635659c4e77ed8732604e4869d955
ibrahimjamil <ibrahimjamil090@gmail.com>
2023-07-15 01:42:40 +0500

    payment add

--- src/modules/payment/payment.controller.ts
+++ src/modules/payment/payment.controller.ts
@@ -1,6 +1,6 @@
 import { Router, Request, Response } from 'express';
 import PaymentService from './payment.service';
-const stripe = require('stripe')('sk_test_26PHem9AhJZvU623DfE1x4sd');
+const stripe = require('stripe')('sk_test_51NTsCuLT8CFim6xhVOFFDpjOUF2s59j9MZCFGSwY3cwNC2xnP5oGEJyxLa6nt39N8yVVlTC2sBZTxA6flx871DEA00px775nMX');
 
 export class PaymentController {
   public router: Router;
@@ -12,17 +12,33 @@ export class PaymentController {
   }
 
   public createPayment = async (req: Request, res: Response) => {
-    // dummy payment success
-    const session = await  stripe.paymentIntents.create({
-      amount: 500,
-      currency: 'gbp',
-      payment_method: 'pm_card_visa',
-    });
-  
-    res.status(200).send({
-      error: false,
-      message: 'Payment Successfull'
-    });
+    const storeItems = new Map([
+      [1, { priceInCents: 10000, name: "PC Hotel" }],
+    ])
+    try {
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ["card"],
+        mode: "payment",
+        line_items: req.body.item.map((item: any) => {
+          const storeItem = storeItems.get(item?.id || 1)
+          return {
+            price_data: {
+              currency: "usd",
+              product_data: {
+                name: storeItem?.name,
+              },
+              unit_amount: storeItem?.priceInCents,
+            },
+            quantity: item?.quantity || 2,
+          }
+        }),
+        success_url: `${process.env.CLIENT_URL}/payment-success`,
+        cancel_url: `${process.env.CLIENT_URL}/payment-fail`,
+      })
+      res.json({ url: session.url })
+    } catch (e: any) {
+      res.status(500).json({ error: e.message })
+    }
   }
 
   public routes() {
